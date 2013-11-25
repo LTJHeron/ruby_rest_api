82f55500dff225e13eab8d03d4297b605cdecf2a
SgtPerm <lukeheron@gmail.com>
2013-11-25 11:30:42 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestApiApp::Application.config.secret_token = '59b42ecc7b4154ee9b49c230819ea7e2107ab879e65ca2138916dffb64fc74f940fe5c50b1268529ea8f97ef191646555fb7ef622c76347fbdebbfe768eaeb81'
